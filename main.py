89574e36d1914b4adcb1b287a7647dfa4f219ffd
Sagor0078 <musagor78@gmail.com>
2024-09-08 09:19:32 +0600

    add app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -39,3 +40,48 @@ async def search_for_user(username:str):
 @app.get('/greet/')
 async def greet(username:Optional[str]="User"):
    return {"message":f"Hello {username}"}
+
+# data validation and data modeling with pydanticHeader
+
+# Request Body : POST, PATCH, PUT, DELETE, or for various other operations.
+
+class UserSchema(BaseModel):
+   username : str
+   email : str
+
+
+@app.post("/create_user")
+async def create_user(user_data : UserSchema):
+   new_user = {
+      "username" : user_data.username,
+      "email" : user_data.email
+   }
+
+   users.append(new_user)
+   return {"message":"User Created successfully","user":new_user}
+
+
+# Request Headers
+
+# inside main.py
+@app.get('/get_headers')
+async def get_all_request_headers(
+    user_agent: Optional[str] = Header(None),
+    accept_encoding: Optional[str] = Header(None),
+    referer: Optional[str] = Header(None),
+    connection: Optional[str] = Header(None),
+    accept_language: Optional[str] = Header(None),
+    host: Optional[str] = Header(None),
+):
+    request_headers = {}
+    request_headers["User-Agent"] = user_agent
+    request_headers["Accept-Encoding"] = accept_encoding
+    request_headers["Referer"] = referer
+    request_headers["Accept-Language"] = accept_language
+    request_headers["Connection"] = connection
+    request_headers["Host"] = host
+
+    return request_headers
+
+
+   
